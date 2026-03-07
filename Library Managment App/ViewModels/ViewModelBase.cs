81ca8f941431d63347b7f0442c67b959d8c125c2
Noah-SDU <nogau25@student.sdu.dk>
2026-03-07 18:44:24 +0100

    MVVM template

---
+++ Library Managment App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Library_Managment_App.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Library Managment App/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Library_Managment_App.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
