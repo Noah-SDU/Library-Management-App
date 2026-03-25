b7a49668f2f692a6e4a7ae03e9fb8653b1dc565c
DTL6 <daavr25@student.sdu.dk>
2026-03-25 19:01:59 +0100

    Edit README.md to have Functional testing examples

--- Library-Managment-App/obj/Debug/net10.0/Library-Managment-App.AssemblyInfo.cs
+++ Library-Managment-App/obj/Debug/net10.0/Library-Managment-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library-Managment-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e02e0953523acf891fbee9b8272b00f35e400c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35d31946b0d75150372bf7f6b2c49e77248977f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library-Managment-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library-Managment-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
